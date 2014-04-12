61c9b5ebfbefa56c593e6c17c6685f4496d1658a
Lingqiang Kong <konglq@gmail.com>
2014-04-12 22:57:07 +0000

    added an about page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       post 'upvote', 'downvote'
     end
   end
+   
+  get 'about' => 'about#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
